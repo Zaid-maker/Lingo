b7e20c13d6534243fce928154b5321a2a265521c
DevMirza <pzhafeez@gmail.com>
2024-03-15 18:05:29 +0000

    fix

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -26,7 +26,7 @@ const buttonVariants = cva(
       size: {
         default: "h-11 px-4 py-2",
         sm: "h-9 px-3",
-        lg: "h-11 px-8",
+        lg: "h-12 px-8",
         icon: "h-10 w-10",
         rounded: "rounded-full",
       },
