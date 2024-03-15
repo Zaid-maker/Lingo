b3a9843e4fde4583e3095fdf3a8bbe9f004424da
DevMirza <pzhafeez@gmail.com>
2024-03-15 18:20:01 +0000

    Marketing

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice, and master new languages with Lingo.
+        </h1>
+      </div>
+    </div>
+  );
+}
