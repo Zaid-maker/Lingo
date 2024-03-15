86851a75695e97ba4ec817642196146795b44ea1
DevMirza <pzhafeez@gmail.com>
2024-03-15 18:03:45 +0000

    Custom Buttons

---
+++ app/buttons/page.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const ButtonsPage = () => {
+  return (
+    <div className="p-4 space-y-4 flex flex-col">
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
+
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
+
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super Outline</Button>
+
+      <Button variant='ghost'>Ghost</Button>
+    </div>
+  );
+};
+
+export default ButtonsPage;
