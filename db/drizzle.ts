5030788760da8f12d5349119a2d96c614ba401ee
DevMirza <pzhafeez@gmail.com>
2024-03-18 15:41:20 +0000

    fix

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -5,6 +5,6 @@ import * as schema from "./schema";
 
 const sql = neon(process.env.DATABASE_URL!);
 // @ts-ignore
-const db = drizzle(sql, [schema]);
+const db = drizzle(sql, { schema });
 
 export default db;
