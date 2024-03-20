bd7be91c5d5ef47b3c013c4ecc8048db28819e8e
DevMirza <pzhafeez@gmail.com>
2024-03-20 17:40:44 +0000

    add more queries

--- db/queries.ts
+++ db/queries.ts
@@ -1,9 +1,37 @@
 import { cache } from "react";
 
 import db from "./drizzle";
+import { eq } from "drizzle-orm";
+import { courses, userProgress } from "@/db/schema";
+import { auth } from "@clerk/nextjs";
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+  });
 
   return data;
 });
