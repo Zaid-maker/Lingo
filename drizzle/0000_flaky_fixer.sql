f00a68dbbdb7566c7a9c47cc4c1076fe9b39e9c8
DevMirza <pzhafeez@gmail.com>
2024-03-21 19:30:03 +0000

    db migration

---
+++ drizzle/0000_flaky_fixer.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS "courses" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"image_src" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "user_progress" (
+	"user_id" text PRIMARY KEY NOT NULL,
+	"user_name" text DEFAULT 'User' NOT NULL,
+	"user_image_src" text DEFAULT '/mascot.svg' NOT NULL,
+	"active_course_id" integer,
+	"hearts" integer DEFAULT 5 NOT NULL,
+	"points" integer DEFAULT 0 NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "user_progress" ADD CONSTRAINT "user_progress_active_course_id_courses_id_fk" FOREIGN KEY ("active_course_id") REFERENCES "courses"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
