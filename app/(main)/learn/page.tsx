147c844db90fb01b09a8be5064e25eb82aa0b43a
DevMirza <pzhafeez@gmail.com>
2024-03-22 20:46:37 +0000

    update learn

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,17 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import { Header } from "./header";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LeanPage = () => {
+const LeanPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourseId) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
