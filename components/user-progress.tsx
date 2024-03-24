e7cbf5cfdbf5049929ed4767bbe7f2ad78d8c5f7
DevMirza <pzhafeez@gmail.com>
2024-03-24 21:17:34 +0000

    show data from db for active user progress course

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
+import { courses } from "@/db/schema";
 
 type Props = {
-  activeCourse: { imageSrc: string; title: string }; // TODO: Replace with DB types
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
